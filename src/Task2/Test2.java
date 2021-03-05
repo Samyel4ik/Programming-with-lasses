5a9b8e629b36d8d7dbfb16214ccad019d954288a
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-03-05 12:16:02 +0300

    T2

--- src/Task2/Test2.java
+++ src/Task2/Test2.java
@@ -1,5 +1,59 @@
 package Task2;
+//M4-T2.Создйте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами.
+// Добавьте конструктор, инициализирующий члены класса по умолчанию. Добавьте set- и get- методы для полей экземпляра класса.
+
 
 public class Test2 {
-    
+    public static void main(String[] args) {
+        Numbers num = new Numbers(5, 20);
+        System.out.println("x = " + num.getX());
+        System.out.println("y = " + num.getY());
+        System.out.println("------------");
+
+        num = new Numbers();
+
+        System.out.println("x = " + num.getX());
+        System.out.println("y = " + num.getY());
+        System.out.println("------------");
+
+        num.setX(111);
+        num.setY(222);
+
+        System.out.println("x = " + num.getX());
+        System.out.println("y = " + num.getY());
+        System.out.println("------------");
+
+    }
 }
+
+class Numbers {
+    private int x, y;
+
+    public Numbers() {
+        x = 1;
+        y = 3;
+    }
+
+    public Numbers(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+
--- src/com/Main.java
+++ src/com/Main.java
@@ -1,14 +1,42 @@
 package com;
 
 public class Main {
-    int x = 5;
-    int y = 10;
-
     public static void main(String[] args) {
-        Main myObj1 = new Main();  // Object 1
-        Main myObj2 = new Main();  // Object 2
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.y);
+        Test2 obj = new Test2();
+        System.out.println("a: " + obj.get_a());
+        System.out.println("b: " + obj.get_b());
+        System.out.println("------------");
+        obj.set_a(123); //obj.a = 123;
+        obj.set_b(43); //obj.b = 43;
+        System.out.println("a: " + obj.get_a());
+        System.out.println("b: " + obj.get_b());
+        System.out.println("------------");
+        obj = new Test2(4, 3);
+        System.out.println("a: " + obj.get_a());
+        System.out.println("b: " + obj.get_b());
+        System.out.println("------------");
     }
 }
-
+class Test2 {
+    private int a, b;
+    public Test2() {
+        a = 1; // Значения по умолчанию
+        b = 5;
+    }
+    public Test2(int a_, int b_) {
+        a = a_;
+        b = b_;
+    }
+    public int get_a() {
+        return a;
+    }
+    public int get_b() {
+        return b;
+    }
+    public void set_a(int a_) {
+        a = a_;
+    }
+    public void set_b(int b_) {
+        b = b_;
+    }
+}
