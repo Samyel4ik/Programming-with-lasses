44c28fbfb3cf672e2004731a3a1d0593c5edb43d
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-03-09 11:39:16 +0300

    T5

--- src/Task5/Class5.java
+++ src/Task5/Class5.java
@@ -1,7 +1,38 @@
 package Task5;
 
 public class Class5 {
+    // Опишите класс, реализующий десятичный счетчик, который может увеличивать или уменьшать свое значение на единицу в заданном диапазоне.
+    // Предусмотрите инициализацию счетчика значениями по умолчанию и произвольными значениями.
+    // Счетчик имеет методы увеличения и уменьшения состояния, и метод позволяющее получить его текущее состояние.
+    // Написать код, демонстрирующий все возможности класса.
+
     public static void main(String[] args) {
 
+        Counter counter = new Counter(0);
+        while (counter.getState() < 10) {
+            System.out.println(counter.getState());
+            counter.increment();
+        }
+        while (counter.getState() > 0) {
+            System.out.println(counter.getState());
+            counter.decrement();
+        }
+    }
+}
+class Counter{
+    int num;
+
+    public Counter (int num){
+        this.num = num;
+
+    }
+    public void increment(){
+        num++;
+    }
+    public void decrement(){
+       num--;
+    }
+    int getState(){
+        return num;
     }
 }
