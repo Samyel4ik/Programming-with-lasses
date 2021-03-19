b0c75658da5475699b3dddc39591a18d544d8f30
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-03-19 13:54:12 +0300

    T8

--- src/Task8/Class8.java
+++ src/Task8/Class8.java
@@ -1,8 +1,5 @@
 package Task8;
 
-import Task9.Book;
-import Task9.BookService;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,32 +11,45 @@ public class Class8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Customer customer1 = new Customer(1, "Petrov", "Ivan", "Ivanovich", 123, 3451);
-
-        Customer[] customers1 = new Customer[]{
-                customer1,
-                new Customer(2, "Sidorov", "Dmitrii", "Sergeevich", 501, 5698),
-                new Customer(3, "Pushkin", "Alexei", "Victorovich", 299, 3000)
-        };
         Customers customersObject = new Customers();    //обьект класса "  сс парамертрами масссива " .изначально пустой
-        customersObject.setCustomers(customers1);       // массив обьекта класса == наполненному масссиву
+        customersObject.setCustomers(arrayCustomers());       // массив обьекта класса == наполненному масссиву с помощью сеттера
+
+        finish(scanner, customersObject);
+
+    }
+
+    public static void finish(Scanner scanner, Customers customersObject) {
         System.out.println("\"Выберете пункт меню:" + "\n" +
                 "1. сортировка фамилии покупателей." + "\n" +
                 "2. список покупателей, у которых номер кредитной карточки находится в заданном интервале.");
         System.out.print("Ваш выбор - ");
         int t = scanner.nextInt();
 
-        if (t == 2) {
-            System.out.print("Введите начало диопазона - ");
-            int x = scanner.nextInt();
-            System.out.print("Введите конец диопазона - ");
-            int y = scanner.nextInt();
-
-            arrayPrint(customersWithCreditCardIn(customersObject, x, y));
-        }
         if (t == 1) {
             arrayPrint(customersSortedAlphabetically(customersObject));
         }
+        if (t == 2) {
+            customersWithCreditCardIn(scanner, customersObject);
+
+        }
+    }
+
+    public static void customersWithCreditCardIn(Scanner scanner, Customers customersObject) {
+        System.out.print("Введите начало диопазона - ");
+        int x = scanner.nextInt();
+        System.out.print("Введите конец диопазона - ");
+        int y = scanner.nextInt();
+
+        arrayPrint(customersWithCreditCardIn(customersObject, x, y));
+    }
+
+    public static Customer[] arrayCustomers() {
+
+        return new Customer[]{
+                new Customer(1, "Petrov", "Ivan", "Ivanovich", 123, 3451),
+                new Customer(2, "Sidorov", "Dmitrii", "Sergeevich", 501, 5698),
+                new Customer(3, "Pushkin", "Alexei", "Victorovich", 299, 3000)
+        };
     }
 
     public static void arrayPrint(Customer[] customers) {
